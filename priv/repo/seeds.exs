870d9d4f32961528775303ffdfb1385e11784a5d
Faymir <fayze@tutamail.com>
2018-11-27 22:28:03 +0100

    Seeds modified to seed database automaticaly with a new user

--- priv/repo/seeds.exs
+++ priv/repo/seeds.exs
@@ -9,3 +9,11 @@
 #
 # We recommend using the bang functions (`insert!`, `update!`
 # and so on) as they will fail if something goes wrong.
+user = (:crypto.strong_rand_bytes(15) |> Base.url_encode64 |> binary_part(0, 15))
+%{
+  email: user <> "@email.com",
+  image: "https://identicapi.herokuapp.com/img/" <> user,
+  password: "secret",
+  username: user
+}
+|> WhatChat.Accounts.create_user
