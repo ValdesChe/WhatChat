770be1978d4d456d8d1d29109e8d42bf19575fa2
ValdoR Che <ingeekteam@gmail.com>
2018-11-29 17:52:47 +0100

    Setting auth

--- assets/auth/index.js
+++ assets/auth/index.js
@@ -3,14 +3,32 @@ const { SCHEME, HOSTNAME } =
   ? {SCHEME: 'http', HOSTNAME: window.location.hostname}
   : {SCHEME: 'http', HOSTNAME: 'localhost:4000'}
 
-const API_URL = `${SCHEME}://${HOSTNAME}/`
+const API_URL = `${SCHEME}://${HOSTNAME}`
 const REGISTRATION_URL = `${API_URL}/accounts/sign_in`
-const LOGIN_URL = `${API_URL}/accounts/log_in;`
+const LOGIN_URL = `${API_URL}/accounts/sign_in`
 const CURRENT_USER_URL = `${API_URL}/accounts/me`
 
 export default {
   user: {
     authenticated: !!window.localStorage.getItem('id_token')
+  },
+
+  login (context, creds, redirect) {
+    context.axios.post(LOGIN_URL, creds)
+      .then(resp => {
+        // window.localStorage.setItem('id_token', resp.body.jwt)
+        console.log(resp);
+        this.user.authenticated = true
+
+        if (redirect) {
+          context.$router.push({path: redirect})
+        }
+      }, resp => {
+        console.log("resp Eroor1"+resp);
+        context.errorServer = resp.data.errors.detail
+      }).catch((err) => {
+         console.log("Erro 2 "+err);
+      })
   },
 
 }
--- assets/components/Login.vue
+++ assets/components/Login.vue
@@ -1,4 +1,112 @@
 <template>
+
+  <el-row :gutter="10" >
+    <el-form :model="ruleForm2" status-icon :rules="rules2" ref="ruleForm2" label-width="120px" class="demo-ruleForm">
+      <h1>Connectez vous !</h1>
+
+      <el-form-item>
+          <p v-if="errorServer !=''" class="error-message">{{ errorServer }}</p>
+      </el-form-item>
+      <el-form-item  prop="email">
+        <el-input type="email" placeholder="Email" v-model="ruleForm2.email" autocomplete="off"></el-input>
+      </el-form-item>
+      <el-form-item prop="password">
+        <el-input type="password" placeholder="Password" v-model="ruleForm2.password" autocomplete="off"></el-input>
+      </el-form-item>
+
+      <el-form-item>
+        <el-button type="primary" @click="submitForm('ruleForm2')">Submit</el-button>
+        <el-button @click="resetForm('ruleForm2')">Reset</el-button>
+
+        <p class="message">
+          Not registered?
+          <router-link to="signup">Create an account</router-link>
+        </p>
+      </el-form-item>
+
+    </el-form>
+  </el-row>
+</template>
+<script>
+import auth from '../auth'
+  export default {
+    data() {
+      var checkEmail = (rule, value, callback) => {
+        if (!value) {
+          return callback(new Error('Please input the email'));
+        }
+        setTimeout(() => {
+          const pattern =/^[\w._-]+[+]?[\w._-]+@[\w.-]+\.[a-zA-Z]{2,10}$/;
+          if (!value.match(pattern)) {
+            callback(new Error('Please enter valid digits.'));
+          } else {
+            if (value.length < 5 ) {
+              callback(new Error('Email must be greater than 5 characters.'));
+            } else {
+              callback();
+            }
+          }
+        }, 1000);
+      };
+      var validatePass = (rule, value, callback) => {
+        if (value === '') {
+          callback(new Error('Please input the password'));
+        } else {
+          if (value.length < 4 ) {
+            callback(new Error('Password must be greater than 4 characters.'));
+          } else {
+            callback();
+          }
+        }
+      };
+
+      return {
+        errorServer: '',
+        ruleForm2: {
+          email: '',
+          password: ''
+        },
+        rules2: {
+
+          email: [
+            { validator: checkEmail, trigger: 'blur' }
+          ],
+          password: [
+            { validator: validatePass, trigger: 'blur' }
+          ]
+
+        }
+      };
+    },
+    methods: {
+      submitForm(formName) {
+        this.$refs[formName].validate((valid) => {
+          if (valid) {
+            //alert('submit!');
+            auth.login(this, this.ruleForm2);
+          } else {
+            console.log('error submit!!');
+            return false;
+          }
+        });
+      },
+      resetForm(formName) {
+        this.$refs[formName].resetFields();
+      }
+    }
+  }
+</script>
+<style scoped>
+  .el-form{
+     width: 600px;
+     padding-top: 10px;
+     margin: 0 auto;
+  }
+</style>
+
+
+<!-- <template>
+
   <div class="login-page">
     <div class="form">
       <p class="redirect-message" v-if="$route.query.redirect">
@@ -95,4 +203,4 @@ export default {
     font-size: 15px;
     color: rgba(190, 64, 62, 0.65);
   }
-</style>
+</style> -->
