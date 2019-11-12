3b1ea1c08f401cc48a3cb329ea46ce3333534b47
Valdes CHE <valdesche@beebay.co>
2019-11-12 01:06:25 +0100

    Use production config from Heroku ENV variables and decrease socket timeout

--- config/prod.exs
+++ config/prod.exs
@@ -11,7 +11,9 @@ use Mix.Config
 # before starting your production server.
 config :whatChat, WhatChatWeb.Endpoint,
   http: [:inet6, port: System.get_env("PORT") || 4000],
-  url: [host: "example.com", port: 80],
+  # url: [host: "example.com", port: 80],
+  url: [scheme: "https", host: "mysterious-meadow-6277", port: 443],
+force_ssl: [rewrite_on: [:x_forwarded_proto]],
   cache_static_manifest: "priv/static/cache_manifest.json"
 
 # Do not print debug messages in production
--- lib/whatChat_web/endpoint.ex
+++ lib/whatChat_web/endpoint.ex
@@ -2,7 +2,7 @@ defmodule WhatChatWeb.Endpoint do
   use Phoenix.Endpoint, otp_app: :whatChat
 
   socket "/socket", WhatChatWeb.UserSocket,
-    websocket: true,
+    websocket: [timeout: 45_000],
     longpoll: false
 
   # Serve at "/" the static files from "priv/static" directory.
