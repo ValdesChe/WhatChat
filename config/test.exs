844d33dbd25694554379ccf60eb087338e373587
Valdes CHE <valdesche@beebay.co>
2020-01-08 12:51:42 +0100

    Update travis configuration

--- config/test.exs
+++ config/test.exs
@@ -13,7 +13,7 @@ config :logger, level: :warn
 config :whatChat, WhatChat.Repo,
   adapter: Ecto.Adapters.Postgres,
   username: "postgres",
-  password: "postgres",
+  password: "",
   database: "whatchat_test",
   hostname: "localhost",
   pool: Ecto.Adapters.SQL.Sandbox
